cf34ec1e8237a7f1d323c3ee617162dc16f72c5e
frizzy1209 <kloecker1997@live.dk>
2022-12-08 20:24:11 +0100

    mindre opdatering

--- main.js
+++ main.js
@@ -2,14 +2,16 @@
 
 const calculateBtn = document.querySelector('.getNumber')
 
-
 calculateBtn.addEventListener('click', function ()
 {
+
     num1 = parseInt(document.querySelector("#firstNumber").value);
     num2 = parseInt(document.querySelector("#secondNumber").value);
-    var sumOfNumbers = num1 * 4 + num2;
-    document.querySelector("#result").innerHTML = sumOfNumbers;
 
-    console.log(sumOfNumbers)
+    var sumOfNumbers = new Intl.NumberFormat('dk-DK').format(num1 * 4 + num2)
+
+    document.querySelector("#result").innerHTML = sumOfNumbers + " kr.";
 })
+
+
 
